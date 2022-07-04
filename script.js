fee85d86dfad079ccbfb4569278e77d48ab99e3b
Arthutepio <arthurodriguesr10@gmail.com>
2022-07-04 15:24:49 -0300

    Requisitos 2 e 3. foram criados 4 div e atribuido a elas uma classe 'color' e um id para cada cor, foi adicionado um estilo a classe color e no script atribuido cores a paleta.

---
+++ script.js
@@ -0,0 +1,8 @@
+let colorBlack = document.getElementById('color-1');
+colorBlack.style.backgroundColor = 'black';
+let colorYellow = document.getElementById('color-2');
+colorYellow.style.backgroundColor = 'yellow';
+let colorBlue = document.getElementById('color-3');
+colorBlue.style.backgroundColor = 'blue';
+let colorGreen = document.getElementById('color-4');
+colorGreen.style.backgroundColor = 'green';
