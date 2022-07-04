c78053d3b63ac4aab166baf09b2d1d3176554e97
Arthutepio <arthurodriguesr10@gmail.com>
2022-07-04 15:45:40 -0300

    Requisito 4. criei uma funçao pra que fosse gerado um quadarado no tamanho 5x5 e apliquei estilo no css.

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ let colorBlue = document.getElementById('color-3');
 colorBlue.style.backgroundColor = 'blue';
 let colorGreen = document.getElementById('color-4');
 colorGreen.style.backgroundColor = 'green';
+
+function criaTag(){
+    let count = [];
+    for (let i = 1; i <= 25; i +=1){
+        count.push(i)
+    }
+    let quadroPixels = document.createElement('div');
+    quadroPixels.id = 'pixel-board';
+    document.body.appendChild(quadroPixels);
+    for(let i in count){
+        let pixel = document.createElement('div');
+        pixel.classList.add('pixel')
+        quadroPixels.appendChild(pixel);
+    }
+    }
+    criaTag()
