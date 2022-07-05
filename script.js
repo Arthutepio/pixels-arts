00c84b0e8134ec08e7b09fb92609e9d52d6ee51c
Arthutepio <arthurodriguesr10@gmail.com>
2022-07-05 08:49:44 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let colorBlack = document.getElementById('color-1');
+const colorBlack = document.getElementById('color-1');
 colorBlack.style.backgroundColor = 'black';
-let colorYellow = document.getElementById('color-2');
+const colorYellow = document.getElementById('color-2');
 colorYellow.style.backgroundColor = 'yellow';
-let colorBlue = document.getElementById('color-3');
+const colorBlue = document.getElementById('color-3');
 colorBlue.style.backgroundColor = 'blue';
-let colorGreen = document.getElementById('color-4');
+const colorGreen = document.getElementById('color-4');
 colorGreen.style.backgroundColor = 'green';
 
-function criaTag(){
+function criaTag() {
     let count = [];
     for (let i = 1; i <= 25; i +=1){
         count.push(i)
@@ -17,8 +17,26 @@ function criaTag(){
     document.body.appendChild(quadroPixels);
     for(let i in count){
         let pixel = document.createElement('div');
-        pixel.classList.add('pixel')
+        pixel.classList.add('pixel');
         quadroPixels.appendChild(pixel);
     }
     }
-    criaTag()
+    criaTag();
+    
+    window.onload = function start() {
+        colorBlack.classList.add('selected')
+    }
+
+    let coresDaPaleta = document.getElementsByClassName("color");
+    
+    function addRemoveSelec(event) {
+        const corSelect = document.querySelector('.selected');
+        corSelect.classList.remove('selected');
+        event.target.classList.add('selected');
+    }
+
+    function clickCorPaleta() {
+        for (let i = 0; i < coresDaPaleta.length; i += 1) {
+          coresDaPaleta[i].addEventListener("click", addRemoveSelec);
+        }
+      } clickCorPaleta();
